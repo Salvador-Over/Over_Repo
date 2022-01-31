00a374a7c8c284d9717b4361a5c1ec9c9957cec1
Salvador Over <rmartinezo@miumg.edu.com>
2022-01-30 20:15:55 -0600

    Se agrega el proyecto

---
+++ LABORATORIO1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int num1,num2;
+    cout << "Introduce un numero" << endl;
+    cin >> num1;
+    cout << "Introduce un numero" << endl;
+    cin >> num2;
+    cout << "La suma es de: " << num1 + num2 << endl;
+
+    int num3,num4;
+    cout << "Introduce un numero" << endl;
+    cin >> num3;
+    cout << "Introduce un numero" << endl;
+    cin >> num4;
+    cout << "La resta es de: " << num3 - num4 << endl;
+
+    int num5,num6;
+    cout << "Introduce un numero" << endl;
+    cin >> num5;
+    cout << "Introduce un numero" << endl;
+    cin >> num6;
+    cout << "La multiplicación es de: " << num5 * num6 << endl;
+
+    int num7,num8;
+    cout << "Introduce un numero" << endl;
+    cin >> num7;
+    cout << "Introduce un numero" << endl;
+    cin >> num8;
+    cout << "La división es de: " << num7 / num8 << endl;
+
+
+    return 0;
+}
