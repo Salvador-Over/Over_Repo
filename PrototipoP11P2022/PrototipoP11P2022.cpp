16d983d4a7ca0343c3428eeb9fd1772d40714c8d
Salvador Over <rmartinezo@miumg.edu.com>
2022-03-03 13:29:46 -0600

    Rama inicial

---
+++ PrototipoP11P2022/PrototipoP11P2022.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define N 6
+main()
+{
+int v1 [N] = {0, 2, 4, 1, 3, 5};
+int v2 [N] = {11, 22, 33, 44, 55, 66};
+int x = 1;
+for(x=0; x <N; x++)
+printf("%d , ", v2[v1[x]]);
+}
