f9c14d35b8fc63a8e2055ad57981d82a40782f6e
Salvador Over <rmartinezo@miumg.edu.com>
2022-03-03 13:42:19 -0600

    Se agrega rama media

---
+++ PrototipoP11P2022/PrototipoP11P2022.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define N 6
+
+main()
+{
+int v1 [N] = {0, 1, 2, 3, 4, 5};
+int v2 [N] = {11, 22, 33, 44, 55, 66};
+int x = 0;
+
+
+for(x=0; x <N; x++)
+printf("%d  , ", v1[v1[x]]);
